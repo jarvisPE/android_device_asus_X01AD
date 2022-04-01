666361b20d8bab534d362c9317ce909ea114f119
Subhendu-droid <55965922+Subhendu-droid@users.noreply.github.com>
2022-04-01 12:40:31 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -91,9 +91,6 @@ PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library
 
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Audio - A2DP
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
