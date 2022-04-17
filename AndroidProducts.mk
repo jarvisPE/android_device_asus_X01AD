19b793c64ebddccd2be398a6996114fee6b6c763
Subhendu-droid <55965922+Subhendu-droid@users.noreply.github.com>
2022-04-17 07:59:04 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_X01AD.mk
+    $(LOCAL_DIR)/lineage_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_X01AD-user \
-    blaze_X01AD-userdebug \
-    blaze_X01AD-eng
+    lineage_X01AD-user \
+    lineage_X01AD-userdebug \
+    lineage_X01AD-eng
